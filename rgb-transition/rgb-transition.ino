76e23436049d53ffac7990ce327c180653c13cdb
Herbert Spencer <herbert.spencer@gmail.com>
2016-11-23 12:49:46 -0300

    rename variables

--- rgb-transition/rgb-transition.ino
+++ rgb-transition/rgb-transition.ino
@@ -3,15 +3,16 @@
     Pedro Garretón - Herbert Spencer
     e[ad]
 */
+
 // input addresses
 const int analogInPin0 = 0;
 const int analogInPin1 = 1;
 const int analogInPin2 = 2;
 
-// output addresses
-const int outR = 9;
-const int outG = 10;
-const int outB = 11;
+// lightput addresses
+const int lightR = 9;
+const int lightG = 10;
+const int lightB = 11;
 
 // potenciometer values
 int potR = 0;
@@ -24,7 +25,7 @@ int lightGval = 0;
 int lightBval = 0;
 
 // switch for different modes
-boolean transitionMode = false;
+boolean transitionMode = true;
 
 // normalized potenciometer values
 float potRnorm = 0;
@@ -32,14 +33,14 @@ float potGnorm = 0;
 float potBnorm = 0;
 
 // color 1
-int colR1 = 23;
-int colG1 = 255;
-int colB1 = 123;
+int colR1 = 255;
+int colG1 = 0;
+int colB1 = 0;
 
 // color 2
-int colR2 = 200;
-int colG2 = 2;
-int colB2 = 230;
+int colR2 = 0;
+int colG2 = 255;
+int colB2 = 255;
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -58,21 +59,21 @@ void loop() {
     potGnorm = potG / 1024.0;
     potBnorm = potB / 1024.0;
 
-    outRval = potRnorm * colR2 + (1 - potRnorm) * colR1;
-    outGval = potRnorm * colG2 + (1 - potRnorm) * colG1;
-    outBval = potRnorm * colB2 + (1 - potRnorm) * colB1;
+    lightRval = potRnorm * colR2 + (1 - potRnorm) * colR1;
+    lightGval = potRnorm * colG2 + (1 - potRnorm) * colG1;
+    lightBval = potRnorm * colB2 + (1 - potRnorm) * colB1;
   }
   else {
     // map values
-    outRval = map(potR, 0, 1023, 255, 0);
-    outGval = map(potG, 0, 1023, 255, 0);
-    outBval = map(potB, 0, 1023, 255, 0);
+    lightRval = map(potR, 0, 1023, 255, 0);
+    lightGval = map(potG, 0, 1023, 255, 0);
+    lightBval = map(potB, 0, 1023, 255, 0);
   }
 
   // write values
-  analogWrite(outR, outRval);
-  analogWrite(outG, outGval);
-  analogWrite(outB, outBval);
+  analogWrite(lightR, lightRval);
+  analogWrite(lightG, lightGval);
+  analogWrite(lightB, lightBval);
 
   // debug();
 }
