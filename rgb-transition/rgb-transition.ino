3268153731651b007992dfc2299a600dbbb242cd
Herbert Spencer <herbert.spencer@gmail.com>
2016-11-23 12:37:39 -0300

    1 knob transition

--- rgb-transition/rgb-transition.ino
+++ rgb-transition/rgb-transition.ino
@@ -3,20 +3,6 @@
     Pedro Garretón - Herbert Spencer
     e[ad]
 */
-
-////color 1///////////////color 2//////
-int R1 = 23;          int R2 = 200;
-int G1 = 255;         int G2 = 2;
-int B1 = 123;         int B2 = 230;
-///////////////////////////////////////
-// temporal interpolated values
-int R = 0;
-int G = 0;
-int B = 0;
-int difR = 0;
-int difG = 0;
-int difB = 0;
-
 // input addresses
 const int analogInPin0 = 0;
 const int analogInPin1 = 1;
@@ -32,42 +18,56 @@ int potR = 0;
 int potG = 0;
 int potB = 0;
 
-// mapped output values
-int outRval = 0;
-int outGval = 0;
-int outBval = 0;
+// mapped lightput values
+int lightRval = 0;
+int lightGval = 0;
+int lightBval = 0;
+
+// switch for different modes
+boolean transitionMode = false;
+
+// normalized potenciometer values
+float potRnorm = 0;
+float potGnorm = 0;
+float potBnorm = 0;
+
+// color 1
+int colR1 = 23;
+int colG1 = 255;
+int colB1 = 123;
+
+// color 2
+int colR2 = 200;
+int colG2 = 2;
+int colB2 = 230;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
 }
 
-
-
-
 void loop() {
+
   // read potenciometer values:
   potR = analogRead(analogInPin0);
   potG = analogRead(analogInPin1);
   potB = analogRead(analogInPin2);
 
-  // map values
-  outRval = map(potR, 0, 1023, 255, 0);
-  outGval = map(potG, 0, 1023, 255, 0);
-  outBval = map(potB, 0, 1023, 255, 0);
+  if (transitionMode) {
+    potRnorm = potR / 1024.0;
+    potGnorm = potG / 1024.0;
+    potBnorm = potB / 1024.0;
 
-/*
- * temporal difference between 
- * current (R) and target(R2);
- */
- 
-  difR = R - R2;
-  difG = G - G2;
-  difB = B - B2;
-  
-  R = R2 * outRval +  difR * R2;
-  G = G2 * outGval;
-  B = B2 * outBval;
+    outRval = potRnorm * colR2 + (1 - potRnorm) * colR1;
+    outGval = potRnorm * colG2 + (1 - potRnorm) * colG1;
+    outBval = potRnorm * colB2 + (1 - potRnorm) * colB1;
+  }
+  else {
+    // map values
+    outRval = map(potR, 0, 1023, 255, 0);
+    outGval = map(potG, 0, 1023, 255, 0);
+    outBval = map(potB, 0, 1023, 255, 0);
+  }
 
   // write values
   analogWrite(outR, outRval);
@@ -82,16 +82,16 @@ void debug() {
   // print the results to the serial monitor:
   Serial.print("R = ");
   Serial.print(potR);
-  //Serial.print("\t out = ");
-  //Serial.print(outputValue);
+  //Serial.print("\t light = ");
+  //Serial.print(lightputValue);
   Serial.print("\t G = ");
   Serial.print(potG);
-  //Serial.print("\t out1 = ");
-  //Serial.println(outGval);
+  //Serial.print("\t light1 = ");
+  //Serial.println(lightGval);
   Serial.print("\t B = ");
   Serial.println(potB);
-  //Serial.print("\t out2 = ");
-  //Serial.println(outBval);
+  //Serial.print("\t light2 = ");
+  //Serial.println(lightBval);
   delay(50);
 
 }
